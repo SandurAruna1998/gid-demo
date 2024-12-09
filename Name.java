5472881f9c808025adfc9afc61ab678f49a20cbd
Sandur Aruna <aruna.s@agilitz.com>
2024-12-09 12:24:24 +0530

    Committing code and name

--- Name.java
+++ Name.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         String firstName = "Sandur";
         String lastName = "Aruna";
-        int age = 26;
+        int age = 25;
         
         System.out.println("My name is: " + firstName + " " + lastName);
         System.out.println("My age is: " + age);
